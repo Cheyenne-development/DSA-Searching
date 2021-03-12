b3539dacfb6f67875ab254aa2d1431c3f7fe783b
Cheyenne McMahon <orlandu8@gmail.com>
2021-03-12 03:46:49 -0600

    first commit

---
+++ binarySearch.js
@@ -0,0 +1,24 @@
+function binarySearch(array, value, start, end) {
+    var start = start === undefined ? 0 : start;
+    var end = end === undefined ? array.length : end;
+
+    if (start > end) {
+        return -1;
+    }
+
+    const index = Math.floor((start + end) / 2);
+    const item = array[index];
+
+    console.log(start, end);
+    if (item == value) {
+        return index;
+    }
+    else if (item < value) {
+        return binarySearch(array, value, index + 1, end);
+    }
+    else if (item > value) {
+        return binarySearch(array, value, start, index - 1);
+    }
+};
+
+console.log(binarySearch([3, 5, 6, 8, 11, 12, 14, 15, 17, 18, 22,26,29,30,32], 30))
